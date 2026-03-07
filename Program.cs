a7d256ac049c9c31fb5e191e7095019fd5237414
lucas <lucashsilvaa8@gmail.com>
2026-03-06 23:27:22 -0300

    configura o banco no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Data;
 using ToDoListAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +12,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Dependency Injection
-builder.Services.AddSingleton<TaskService>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<TaskService>();
 
 var app = builder.Build();
 
