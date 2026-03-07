2383ef2a1f9a0aaf938a14998554e6ea18d97cd1
lucas <lucashsilvaa8@gmail.com>
2026-03-06 22:37:21 -0300

    configura Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using ToDoListAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Dependency Injection
+builder.Services.AddSingleton<TaskService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
